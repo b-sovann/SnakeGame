0c5a637afa175140d74291e2a423f14d53b0fc68
Bondith Sovann <bondithsovann004@gmail.com>
2021-05-14 22:06:08 -0500

    Succeed with fruit implementation for Snake Game!!

--- SnakeGame/src/WallConflict.java
+++ SnakeGame/src/WallConflict.java
@@ -14,6 +14,7 @@ import java.util.Random;
 
 public class WallConflict {
     static Screen screen = null;
+
     public static void main(String[] args) {
         DefaultTerminalFactory defaultTerminalFactory = new DefaultTerminalFactory();
         Screen screen = null;
@@ -31,14 +32,14 @@ public class WallConflict {
 
             TerminalSize terminalSize = terminal.getTerminalSize();
 
-            int width  = terminalSize.getColumns();
+            int width = terminalSize.getColumns();
             int height = terminalSize.getRows();
             int[] fruitCoordinate = WallConflict.getFruit(width, height);
 
-            while(true) {
+            while (true) {
                 KeyStroke keyStroke = screen.pollInput();
-                if(keyStroke != null) {
-                    if(keyStroke.getKeyType() == KeyType.ArrowLeft) {
+                if (keyStroke != null) {
+                    if (keyStroke.getKeyType() == KeyType.ArrowLeft) {
                         direction = 0;
                         WallConflict.putCharrEmpty(x, y);
                         x--;
@@ -46,7 +47,7 @@ public class WallConflict {
                             fruitCoordinate = WallConflict.getFruit(width, height);
                         }
                     }
-                    if(keyStroke.getKeyType() == KeyType.ArrowUp) {
+                    if (keyStroke.getKeyType() == KeyType.ArrowUp) {
                         direction = 1;
                         WallConflict.putCharrEmpty(x, y);
                         y--;
@@ -54,7 +55,7 @@ public class WallConflict {
                             fruitCoordinate = WallConflict.getFruit(width, height);
                         }
                     }
-                    if(keyStroke.getKeyType() == KeyType.ArrowRight) {
+                    if (keyStroke.getKeyType() == KeyType.ArrowRight) {
                         direction = 2;
                         WallConflict.putCharrEmpty(x, y);
                         x++;
@@ -62,23 +63,22 @@ public class WallConflict {
                             fruitCoordinate = WallConflict.getFruit(width, height);
                         }
                     }
-                    if(keyStroke.getKeyType() == KeyType.ArrowDown) {
+                    if (keyStroke.getKeyType() == KeyType.ArrowDown) {
                         direction = 3;
                         WallConflict.putCharrEmpty(x, y);
                         y++;
-                            if (x == fruitCoordinate[0] && y == fruitCoordinate[1]) {
-                                fruitCoordinate = WallConflict.getFruit(width, height);
-                            }
+                        if (x == fruitCoordinate[0] && y == fruitCoordinate[1]) {
+                            fruitCoordinate = WallConflict.getFruit(width, height);
+                        }
                     }
                 }
 
                 WallConflict.drawWall(width, height);
                 WallConflict.putCharr(x, y, heads[direction]);
-                WallConflict.putCharr( fruitCoordinate[0], fruitCoordinate[1], '@');
+                WallConflict.putCharr(fruitCoordinate[0], fruitCoordinate[1], '@');
 
 
-
-                if (WallConflict.hitWall(x, y, width, height)){
+                if (WallConflict.hitWall(x, y, width, height)) {
                     System.out.println("Hit");
                 }
 
@@ -87,65 +87,62 @@ public class WallConflict {
             }
 
 
-
-        }
-        catch(IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
-        }
-        finally {
-            if(screen != null) {
+        } finally {
+            if (screen != null) {
                 try {
                     screen.close();
-                }
-                catch(IOException e) {
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
     }
+
     private static void putCharrEmpty(int x, int y) {
         WallConflict.putCharr(x, y, ' ');
     }
+
     private static void putCharr(int x, int y, char c) {
         WallConflict.screen.setCharacter(x, y, new TextCharacter(
-                            c,
-                            TextColor.ANSI.DEFAULT,
-                            // This will pick a random background color
-                            TextColor.ANSI.values()[0]));
+                c,
+                TextColor.ANSI.DEFAULT,
+                // This will pick a random background color
+                TextColor.ANSI.values()[0]));
     }
 
-    private static void drawWall(int width, int height){
+    private static void drawWall(int width, int height) {
 
-        for (int i =0; i < width; i++){
+        for (int i = 0; i < width; i++) {
             WallConflict.putCharr(i, 0, '*');
         }
-        for (int i =0; i < width; i++){
-            WallConflict.putCharr(i, height-1, '*');
+        for (int i = 0; i < width; i++) {
+            WallConflict.putCharr(i, height - 1, '*');
         }
-        for (int i =0; i < height; i++){
+        for (int i = 0; i < height; i++) {
             WallConflict.putCharr(0, i, '*');
         }
-        for (int i =0; i < height; i++){
-            WallConflict.putCharr(width-1, i, '*');
+        for (int i = 0; i < height; i++) {
+            WallConflict.putCharr(width - 1, i, '*');
         }
     }
 
-    private static boolean hitWall(int x, int y, int width, int height){
+    private static boolean hitWall(int x, int y, int width, int height) {
         boolean isHit = false;
-        if (x == width-1 || y == height -1 || x == 0 || y == 0)
+        if (x == width - 1 || y == height - 1 || x == 0 || y == 0)
             isHit = true;
         return isHit;
     }
 
-    private static int[] getFruit(int width, int height){
+    private static int[] getFruit(int width, int height) {
         int[] fruit = new int[2];
         Random random = new Random();
 
-            fruit[0] = random.nextInt(width);
-            fruit[1] = random.nextInt(height);
+        fruit[0] = random.nextInt(width - 2) + 1;
+        fruit[1] = random.nextInt(height - 2) + 1;
         return fruit;
     }
-
 
 
 }
